53b78fbb643391e05189b077f11e6d76c832e44f
StevenYulongYan <yulong.yan@uqconnect.edu.au>
2021-06-09 10:30:29 +1000

    Animate switch

--- components/Buttons/SlideSelect/index.tsx
+++ components/Buttons/SlideSelect/index.tsx
@@ -7,30 +7,45 @@ type TSSProps = {
     value: number;
     className?: string;
 } & Children;
-
 const SlideSelect: React.FC<TSSProps> = styled(({ onClick, value, children, className }: TSSProps) => {
     return (
-        <>
-            <div className={className}>
-                {React.Children.toArray(children).map((child, index) => {
-                    return (
-                        <SlideOption
-                            onClick={(e) => onClick(index, e)}
-                            key={`slide-option-${index}`}
-                            className={`${index === value ? 'selected' : ''}`}
-                        >
-                            {child}
-                        </SlideOption>
-                    );
-                })}
-            </div>
-        </>
+        <div className={className}>
+            {React.Children.toArray(children).map((child, index) => {
+                return (
+                    <SlideOption
+                        onClick={(e) => onClick(index, e)}
+                        key={`slide-option-${index}`}
+                        className={`${index === value ? 'selected' : ''}`}
+                    >
+                        {child}
+                    </SlideOption>
+                );
+            })}
+            <div className="bg" />
+        </div>
     );
 })`
     display: flex;
     margin: auto;
     border: 1px solid #3da8f5;
     border-radius: 20px;
+    height: 40px;
+    position: relative;
+
+    .bg {
+        transition: 0.5s;
+        background-color: #3da8f5;
+        height: 39px;
+        width: 170px;
+        border-radius: 18px;
+        position: absolute;
+        top: 0;
+    }
+
+    .selected + .bg {
+        transition: 0.5s;
+        margin-left: 128px;
+    }
 `;
 
 export const SlideOption = styled.div`
@@ -39,18 +54,10 @@ export const SlideOption = styled.div`
     font-size: 16px;
     text-align: center;
     width: 100%;
-    color: #3da8f5;
-
-    transition: background 0.3;
+    z-index: 1;
 
     &:hover {
         cursor: pointer;
-    }
-
-    &.selected {
-        background: #3da8f5;
-        color: #fff;
-        border: 1px solid #3da8f5;
     }
 `;
 
