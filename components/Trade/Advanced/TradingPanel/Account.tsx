0fdc02fafed20046d43a8208a93685b353707264
StevenYulongYan <yulong.yan@uqconnect.edu.au>
2021-06-15 10:05:09 +1000

    Resolve merge conflict

--- components/Trade/Advanced/TradingPanel/Account.tsx
+++ components/Trade/Advanced/TradingPanel/Account.tsx
@@ -2,14 +2,18 @@ import React, { useContext, useState } from 'react';
 import { Tracer } from 'libs';
 import { toApproxCurrency } from '@libs/utils';
 import styled from 'styled-components';
-import { calcTotalMargin, calcMinimumMargin } from '@tracer-protocol/tracer-utils';
+import { calcTotalMargin, calcMinimumMargin, calcLeverage, calcLiquidationPrice } from '@tracer-protocol/tracer-utils';
 import { Box, Button } from '@components/General';
-import { Web3Context } from 'context';
+import { Web3Context, TracerContext } from 'context';
 import { BigNumber } from 'bignumber.js';
 import { defaults } from '@libs/Tracer';
 import AccountModal from './AccountModal';
-
-const MinHeight = 250;
+import DefaultSlider from '@components/Slider';
+import { UserBalance } from 'types';
+import { SlideSelect } from '@components/Buttons';
+import TracerModal from '@components/Modals';
+import { NumberSelect } from '@components/General';
+import { Option } from '@components/Buttons/SlideSelect';
 
 const SBox = styled(Box)`
     background: #011772;
@@ -17,7 +21,7 @@ const SBox = styled(Box)`
     display: flex;
     flex-direction: column;
     justify-content: center;
-    min-height: ${MinHeight}px;
+    min-height: 250px;
     z-index: 4;
 
     > p {
@@ -89,10 +93,227 @@ const SButton = styled(Button)`
     padding: 0;
 `;
 
+const CalcSelectContainer = styled.div`
+    margin-top: 1rem;
+    border-top: 1px solid #002886;
+`;
+
+const CalcSlideSelect = styled(SlideSelect)`
+    max-width: 300px;
+    margin: 1rem auto;
+`;
+
+const CalcButtons = styled.div`
+    margin: 3rem 5rem;
+    display: flex;
+    justify-content: space-around;
+`;
+
+const AccountNumberSelect = styled(NumberSelect)`
+    margin-top: 1rem;
+    > * .balance {
+        color: #3da8f5;
+        margin-left: 2rem;
+    }
+    > * .balance > .max {
+        margin-left: 2rem;
+    }
+`;
+
+type LProps = {
+    leverage: number;
+    className?: string;
+};
+const Leverage: React.FC<LProps> = styled(({ className, leverage }: LProps) => {
+    return (
+        <div className={className}>
+            <h3>Leverage</h3>
+            <DefaultSlider className="px-5" value={leverage ?? 1} />
+        </div>
+    );
+})`
+    display: flex;
+    flex-direction: column;
+    margin-top: 2rem;
+    margin-bottom: 4rem;
+
+    > h3 {
+        font-size: 1rem;
+        color: #3da8f5;
+        margin-bottom: 1rem;
+    }
+`;
+
+type CalculatorModalProps = {
+    className?: string;
+    close: () => any;
+    display: boolean;
+    exposureUnit: string;
+    marginUnit: string;
+    balances: UserBalance;
+    price: BigNumber;
+};
+const CalculatorModal: React.FC<CalculatorModalProps> = styled(
+    ({ className, close, exposureUnit, marginUnit, balances, display }: CalculatorModalProps) => {
+        const { selectedTracer } = useContext(TracerContext);
+        const [leverage] = useState(1);
+        const [exposureAmount, setExposureAmount] = useState(NaN);
+        const [marginAmount, setMarginAmount] = useState(NaN);
+        const [liquidationAmount, setLiquidationAmount] = useState(NaN);
+
+        const [isLong, setPosition] = useState(true);
+        const [showResult, setShowResult] = useState(false);
+
+        const [exposureLocked, setExposureLocked] = useState(false);
+        const [marginLocked, setMarginLocked] = useState(false);
+
+        const Calculate = () => {
+            if (isLong) {
+                setLiquidationAmount(
+                    calcLiquidationPrice(
+                        new BigNumber(marginAmount).negated(),
+                        new BigNumber(exposureAmount),
+                        new BigNumber(1),
+                        new BigNumber(25),
+                    ).toNumber(),
+                );
+            } else {
+                setLiquidationAmount(
+                    calcLiquidationPrice(
+                        new BigNumber(marginAmount).negated(),
+                        new BigNumber(-Math.abs(exposureAmount)),
+                        new BigNumber(1),
+                        new BigNumber(25),
+                    ).toNumber(),
+                );
+            }
+            setShowResult(true);
+        };
+
+        const Reset = () => {
+            setExposureAmount(NaN);
+            setMarginAmount(NaN);
+            setLiquidationAmount(NaN);
+            setShowResult(false);
+
+            setExposureLocked(false);
+            setMarginLocked(false);
+        };
+
+        const ChangeExposure = (e: any) => {
+            setExposureAmount(Math.abs(parseFloat(e.target.value)));
+            setShowResult(false);
+        };
+
+        const ChangeMargin = (e: any) => {
+            setMarginAmount(Math.abs(parseFloat(e.target.value)));
+            setShowResult(false);
+        };
+
+        const ChangeLiquidation = (e: any) => {
+            setLiquidationAmount(Math.abs(parseFloat(e.target.value)));
+            setShowResult(false);
+        };
+
+        return (
+            <TracerModal
+                loading={false}
+                className={className}
+                show={display}
+                title={`${selectedTracer?.marketId} Calculator`}
+                onClose={close}
+            >
+                <CalcSelectContainer>
+                    <CalcSlideSelect value={isLong ? 0 : 1} onClick={() => setPosition(!isLong)}>
+                        <Option>Long</Option>
+                        <Option>Short</Option>
+                    </CalcSlideSelect>
+                </CalcSelectContainer>
+
+                <AccountNumberSelect
+                    unit={exposureUnit}
+                    title={'Exposure'}
+                    amount={exposureAmount}
+                    setAmount={setExposureAmount}
+                    onChange={ChangeExposure}
+                    hasLock={true}
+                    isLocked={exposureLocked}
+                    lockOnClick={() => setExposureLocked(!exposureLocked)}
+                />
+
+                <AccountNumberSelect
+                    unit={marginUnit}
+                    title={'Margin'}
+                    amount={marginAmount}
+                    balance={balances.tokenBalance.toNumber()}
+                    setAmount={setMarginAmount}
+                    onChange={ChangeMargin}
+                    hasLock={true}
+                    isLocked={marginLocked}
+                    lockOnClick={() => setMarginLocked(!marginLocked)}
+                />
+
+                <Leverage leverage={leverage} />
+
+                <div>
+                    Leverage:{' '}
+                    {showResult
+                        ? isLong
+                            ? calcLeverage(
+                                  new BigNumber(marginAmount).negated(),
+                                  new BigNumber(exposureAmount),
+                                  new BigNumber(1),
+                              ).toNumber()
+                            : calcLeverage(
+                                  new BigNumber(marginAmount).negated(),
+                                  new BigNumber(-Math.abs(exposureAmount)),
+                                  new BigNumber(1),
+                              ).toNumber()
+                        : null}
+                </div>
+
+                <div>
+                    Liquidation Price:{' '}
+                    {showResult
+                        ? isLong
+                            ? calcLiquidationPrice(
+                                  new BigNumber(marginAmount).negated(),
+                                  new BigNumber(exposureAmount),
+                                  new BigNumber(1),
+                                  new BigNumber(25),
+                              ).toNumber()
+                            : calcLiquidationPrice(
+                                  new BigNumber(marginAmount).negated(),
+                                  new BigNumber(-Math.abs(exposureAmount)),
+                                  new BigNumber(1),
+                                  new BigNumber(25),
+                              ).toNumber()
+                        : null}
+                </div>
+
+                <AccountNumberSelect
+                    unit={marginUnit}
+                    title={'Liquidation Price'}
+                    amount={liquidationAmount}
+                    balance={balances.tokenBalance.toNumber()}
+                    setAmount={setLiquidationAmount}
+                    onChange={ChangeLiquidation}
+                />
+
+                <CalcButtons>
+                    <SButton onClick={Calculate}>Calculate</SButton>
+                    <SButton onClick={Reset}>Reset</SButton>
+                </CalcButtons>
+            </TracerModal>
+        );
+    },
+)``;
+
 const AccountPanel: React.FC<{
     selectedTracer: Tracer | undefined;
     account: string;
 }> = ({ selectedTracer, account }) => {
+    const [calculator, showCalculator] = useState(false);
     const [popup, setPopup] = useState(false);
     const [deposit, setDeposit] = useState(false);
     const balances = selectedTracer?.getBalance() ?? defaults.balances;
@@ -109,7 +330,12 @@ const AccountPanel: React.FC<{
     ) : (
         <AccountInfo>
             <Item>
-                <h3>Total Margin</h3>
+                <div className="flex">
+                    <h3>Total Margin</h3>
+                    <SButton className="ml-auto mr-1" onClick={() => showCalculator(true)}>
+                        Calculator
+                    </SButton>
+                </div>
                 <span>
                     <a>{toApproxCurrency(calcTotalMargin(balances.quote, balances.base, fairPrice))}</a>
                 </span>
@@ -132,6 +358,14 @@ const AccountPanel: React.FC<{
                 unit={selectedTracer?.marketId?.split('/')[1] ?? 'NO_ID'}
                 balances={balances}
                 maxLeverage={maxLeverage}
+                price={Number.isNaN(fairPrice) ? 0 : fairPrice}
+            />
+            <CalculatorModal
+                display={calculator}
+                close={() => showCalculator(false)}
+                exposureUnit={selectedTracer?.marketId?.split('/')[0] ?? 'NO_ID'}
+                marginUnit={selectedTracer?.marketId?.split('/')[1] ?? 'NO_ID'}
+                balances={balances}
                 price={Number.isNaN(fairPrice) ? 0 : fairPrice}
             />
         </AccountInfo>
