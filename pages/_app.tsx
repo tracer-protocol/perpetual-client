1a2c83e4d0ad0a86262225cd8a197c5672161ebe
StevenYulongYan <yulong.yan@uqconnect.edu.au>
2021-06-17 11:40:09 +1000

    Try adding usersnap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,9 @@ import { Notification } from '@components/General/Notification';
 import { TransactionStore } from '@context/TransactionContext';
 import { FactoryStore } from '@context/FactoryContext';
 
+const USERSNAP_GLOBAL_API_KEY = '4a12cbf7-62f5-4cf4-be95-95526eadbcbb';
+const USERSNAP_API_KEY = '75dd7bec-5327-452a-bb9d-cc8755f18bce';
+
 const App = ({ Component, pageProps }: AppProps) => { // eslint-disable-line
     return (
         <div>
@@ -32,6 +35,23 @@ const App = ({ Component, pageProps }: AppProps) => { // eslint-disable-line
 
                 {/** Color for Chrome tabs (Android only) */}
                 <meta name="theme-color" content="#000240" />
+
+                <script
+                    async
+                    src={`https://widget.usersnap.com/global/load/${USERSNAP_GLOBAL_API_KEY}?onload=onUsersnapCXLoad`}
+                />
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+              window.onUsersnapCXLoad = function(api) {
+                // store the Usersnap global api on the window, if case you want to use it in other contexts
+                window.Usersnap = api; 
+                api.init();
+                api.show('${USERSNAP_API_KEY}') 
+            }         
+            `,
+                    }}
+                />
             </Head>
             <ToastProvider components={{ Toast: Notification }}>
                 <Web3Store>
