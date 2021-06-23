9d4b4fa358b20f3e42ec6fc921d0018cf6376d1c
Aidan Tetther <tetthera@gmail.com>
2021-06-23 11:09:41 +1000

    New nav dropdown (#130)
    * Update navbar
    
    * lint and build

--- components/Nav/Navbar/index.tsx
+++ components/Nav/Navbar/index.tsx
@@ -7,6 +7,7 @@ import styled from 'styled-components';
 // @ts-ignore
 import ENS, { getEnsAddress } from '@ensdomains/ensjs';
 import { Web3Context } from 'context';
+import HeaderSiteSwitcher from './HeaderSiteSwitcher';
 
 // const NetworkButton = styled.span`
 //     border: 1px solid #fff;
@@ -84,188 +85,6 @@ const useEnsName = (account: string) => {
 
 const Identicon = dynamic(import('../Identicon'), { ssr: false });
 
-const DropdownLogo = styled(({ className }) => {
-    const [show, setShow] = useState(false);
-
-    useEffect(() => {
-        document.addEventListener('click', (e) => {
-            const menu = document.getElementById('menu');
-            let target = e.target;
-            do {
-                if (target === menu) {
-                    return;
-                }
-                // @ts-ignore
-                target = target?.parentNode;
-            } while (target);
-            setShow(false);
-        });
-    }, []);
-
-    const handleClick = (e: any) => {
-        e.preventDefault();
-        setShow(!show);
-        const dropdown = document.querySelector('.dropdown-menu-list');
-        dropdown?.addEventListener('transitionend', () => {
-            // add delay after open
-            if (document.getElementById('menu')?.classList.contains('show')) {
-                dropdown?.classList.add('delayed-transition');
-            }
-        });
-        dropdown?.addEventListener('transitionend', () => {
-            // remove delay after close
-            if (!document.getElementById('menu')?.classList.contains('show')) {
-                dropdown?.classList.remove('delayed-transition');
-            }
-        });
-    };
-    return (
-        <div id="menu" className={`${className} ${show ? 'show' : ''}`} onClick={handleClick}>
-            <img alt="Tracer Logo" className="logo hidden lg:block z-10" src="/img/logos/tracer/tracer_perps.svg" />
-            <img alt="down-arrow" className="down-arrow z-10" src="/img/general/triangle_down.svg" />
-            <div className={`dropdown-menu`}>
-                <ul className="dropdown-menu-list">
-                    <li className="nav-item" />
-                    <li className="nav-item highlight">
-                        <img alt="Tracer Logo" src="/img/logos/tracer/tracer_main.svg" />
-                    </li>
-                    <li className="nav-item highlight">
-                        <img alt="Tracer Logo" src="/img/logos/tracer/tracer_govern.svg" />
-                    </li>
-                    <li className="nav-item highlight">
-                        <img alt="Tracer Logo" src="/img/logos/tracer/tracer_blog.svg" />
-                    </li>
-                </ul>
-            </div>
-        </div>
-    );
-})`
-    height: 100%;
-    display: flex;
-    width: 350px;
-    position: relative;
-
-    &:hover {
-        cursor: pointer;
-    }
-
-    > .logo {
-        height: 30px;
-        margin-top: 3vh;
-    }
-
-    > .down-arrow {
-        height: 30px;
-        width: 30px;
-        top: 2.5vh;
-        right: 0;
-        position: absolute;
-        margin-top: 0;
-        transition: 0.5s;
-    }
-
-    &.show > .down-arrow {
-        transform: rotate(180deg);
-        margin-top: 10px;
-    }
-
-    > .dropdown-menu {
-        position: absolute;
-        opacity: 0;
-        height: 0;
-        top: 10px;
-        left: -30px;
-        width: 400px;
-        background: #3372e8;
-        border-radius: 10px;
-        transition: all 0.3s ease-in-out;
-        z-index: -1;
-    }
-
-    > .dropdown-menu ul {
-        margin-top: 3vh;
-        transition: all 0.3s ease-in-out;
-        background: #3372e8;
-        border-radius: 10px;
-        opacity: 0;
-    }
-
-    .delayed-transition {
-        transition-delay: 0.3s !important;
-    }
-
-    > .dropdown-menu ul .nav-item {
-        background: transparent;
-        border-top: transparent;
-        padding: 20px 0;
-        width: 100%;
-        transform: translateX(20px);
-        opacity: 0;
-    }
-
-    > .dropdown-menu ul img {
-        height: 30px;
-    }
-
-    .dropdown-menu ul .nav-item:nth-child(1) {
-        border-top: transparent;
-        height: 70px; // 30px for logo and 40px for padding;
-        border-top-left-radius: 10px;
-        border-top-right-radius: 10px;
-    }
-
-    > .dropdown-menu ul .nav-item:nth-child(2) {
-        transition: all 0.3s ease-in-out, background 0.5s ease;
-    }
-
-    > .dropdown-menu ul .nav-item:nth-child(3) {
-        transition: all 0.3s ease-in-out, background 0.5s ease;
-    }
-    > .dropdown-menu ul .nav-item:nth-child(4) {
-        transition: all 0.3s ease-in-out, background 0.5s ease;
-    }
-
-    &.show > .dropdown-menu ul .nav-item:nth-child(2) {
-        transition: all 0.3s ease-in-out 0.2s, background 0.5s ease;
-    }
-
-    &.show > .dropdown-menu ul .nav-item:nth-child(3) {
-        transition: all 0.3s ease-in-out 0.3s, background 0.5s ease;
-    }
-
-    &.show > .dropdown-menu ul .nav-item:nth-child(4) {
-        transition: all 0.3s ease-in-out 0.4s, background 0.5s ease;
-        border-bottom-left-radius: 10px;
-        border-bottom-right-radius: 10px;
-    }
-
-    &.show > .dropdown-menu ul .nav-item.highlight {
-        border-top: 1px solid var(--color-primary);
-        transform: translateX(0);
-        opacity: 1;
-
-        &:hover {
-            cursor: pointer;
-            background-color: var(--color-primary);
-        }
-    }
-
-    &.show > .dropdown-menu ul {
-        margin-top: calc(3vh - 30px);
-        opacity: 1;
-    }
-
-    .dropdown-menu ul .nav-item img {
-        margin-left: 30px;
-    }
-
-    &.show > .dropdown-menu {
-        opacity: 1;
-        height: 280px;
-        z-index: 5;
-    }
-`;
-
 const ConnectButton: React.FC<any> = styled.button`
     display: flex;
     border: 2px solid #fff;
@@ -327,7 +146,7 @@ const NavBarContent: React.FC = styled(({ className }) => {
 
     return (
         <nav className={`${className} container`}>
-            <DropdownLogo />
+            <HeaderSiteSwitcher />
             <ul>
                 <li className={linkStyles + (route === 'trade' ? ' selected' : '')}>
                     {/*TODO: Add back basic/advanced trading toggle (change the file name and path config too)*/}
