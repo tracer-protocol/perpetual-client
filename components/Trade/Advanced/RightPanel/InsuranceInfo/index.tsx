90a3e9078db28cb556cb640808bf4673da364a2d
StevenYulongYan <yulong.yan@uqconnect.edu.au>
2021-07-07 15:08:49 +1000

    Insurance health overlay

--- components/Trade/Advanced/RightPanel/InsuranceInfo/index.tsx
+++ components/Trade/Advanced/RightPanel/InsuranceInfo/index.tsx
@@ -1,68 +1,81 @@
-import React, { useContext } from 'react';
+import React, { FC, useContext, useState } from 'react';
 import { toApproxCurrency } from 'libs/utils';
 import styled from 'styled-components';
 import { ProgressBar, Section } from '@components/General';
 import { InsuranceContext, defaults } from '@context/InsuranceContext';
 import TooltipSelector from '@components/Tooltips/TooltipSelector';
 import BigNumber from 'bignumber.js';
+import FogOverlay from '@components/Overlay/FogOverlay';
 
-export default styled(({ fundingRate, className }) => {
+interface IIProps {
+    className?: string;
+    fundingRate: BigNumber;
+}
+const InsuranceInfo: FC<IIProps> = styled(({ className, fundingRate }: IIProps) => {
     const { poolInfo } = useContext(InsuranceContext);
     const poolHealth = poolInfo?.health ?? defaults.health;
     const poolLiquidity = poolInfo?.liquidity ?? defaults.liquidity;
     const poolTarget = poolInfo?.target ?? defaults.target;
+    const [showOverlay, setOverlay] = useState(true);
 
     return (
         <div className={className}>
-            <h3>
-                <TooltipSelector tooltip={{ key: 'insurance-pool-health' }}>Insurance Pool Health</TooltipSelector>
-            </h3>
-            <ProgressBar percent={parseFloat(poolHealth.toFixed(2)) ?? defaults.health} />
-            <Section
-                label="Pool Holdings"
-                tooltip={{
-                    key: `pool-holdings`,
-                    props: {
-                        baseTicker: poolInfo?.market?.split('/')[0],
-                    },
-                }}
-            >
-                {toApproxCurrency(poolLiquidity ?? defaults.liquidity)}
-            </Section>
-            <Section
-                label="Pool Target"
-                tooltip={{
-                    key: `pool-target`,
-                    props: {
-                        baseTicker: poolInfo?.market?.split('/')[0],
-                    },
-                }}
-            >
-                {toApproxCurrency(poolTarget ?? defaults.target)}
-            </Section>
-            <Section
-                label="Insurance Funding Rate"
-                tooltip={{
-                    key: `insurance-funding-rate`,
-                    props: {
-                        baseTicker: poolInfo?.market?.split('/')[0],
-                    },
-                }}
-            >
-                {(fundingRate.toNumber() * 100).toFixed(5)}%
-            </Section>
+            <InfoContent>
+                <InfoTitle>
+                    <TooltipSelector tooltip={{ key: 'insurance-pool-health' }}>Insurance Pool Health</TooltipSelector>
+                </InfoTitle>
+                <ProgressBar percent={parseFloat(poolHealth.toFixed(2)) ?? defaults.health} />
+                <Section
+                    label="Pool Holdings"
+                    tooltip={{
+                        key: `pool-holdings`,
+                        props: {
+                            baseTicker: poolInfo?.market?.split('/')[0],
+                        },
+                    }}
+                >
+                    {toApproxCurrency(poolLiquidity ?? defaults.liquidity)}
+                </Section>
+                <Section
+                    label="Pool Target"
+                    tooltip={{
+                        key: `pool-target`,
+                        props: {
+                            baseTicker: poolInfo?.market?.split('/')[0],
+                        },
+                    }}
+                >
+                    {toApproxCurrency(poolTarget ?? defaults.target)}
+                </Section>
+                <Section
+                    label="Insurance Funding Rate"
+                    tooltip={{
+                        key: `insurance-funding-rate`,
+                        props: {
+                            baseTicker: poolInfo?.market?.split('/')[0],
+                        },
+                    }}
+                >
+                    {(fundingRate.toNumber() * 100).toFixed(5)}%
+                </Section>
+            </InfoContent>
+            {showOverlay ? <FogOverlay buttonName="Show Chart" onClick={() => setOverlay(false)} /> : null}
         </div>
     );
-})<{
-    fundingRate: BigNumber;
-}>`
-    padding: 10px;
-    h3 {
-        font-size: var(--font-size-medium);
-        letter-spacing: -0.4px;
-        color: #ffffff;
-        text-transform: capitalize;
-    }
-
+})`
+    position: relative;
     max-height: 25vh;
+`;
+
+export default InsuranceInfo;
+
+const InfoContent = styled.div`
+    padding: 10px;
+`;
+
+const InfoTitle = styled.div`
+    font-size: var(--font-size-medium);
+    letter-spacing: -0.4px;
+    color: #ffffff;
+    text-transform: capitalize;
 `;
