72d529e3a6537a1bf557aa780ca375962072fe49
StevenYulongYan <yulong.yan@uqconnect.edu.au>
2021-07-08 10:29:44 +1000

    Show fog overlay only when placing an order

--- components/Overlay/FogOverlay/index.tsx
+++ components/Overlay/FogOverlay/index.tsx
@@ -1,21 +1,27 @@
-import React, { FC } from 'react';
+import React, { FC, useContext } from 'react';
 import Overlay from '@components/Overlay';
 import styled from 'styled-components';
 import { Button } from '@components/General';
+import { OrderContext } from '@context/OrderContext';
 
 interface FOProps {
     buttonName: string;
     onClick: () => void;
 }
 const FogOverlay: FC<FOProps> = ({ buttonName, onClick }: FOProps) => {
+    const { order } = useContext(OrderContext);
     return (
-        <Overlay>
+        <StyledOverlay show={!!order?.exposureBN.toNumber()}>
             <ShowButton onClick={onClick}>{buttonName}</ShowButton>
-        </Overlay>
+        </StyledOverlay>
     );
 };
 
 export default FogOverlay;
+
+const StyledOverlay = styled(Overlay)<{ show: boolean }>`
+    display: ${(props) => (props.show ? 'flex' : 'none')};
+`;
 
 const ShowButton = styled(Button)`
     height: var(--height-small-button);
