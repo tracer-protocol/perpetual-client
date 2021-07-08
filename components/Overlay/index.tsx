80ac171bc150a1f8b8d6a70882356ae56d915e90
StevenYulongYan <yulong.yan@uqconnect.edu.au>
2021-07-08 15:26:37 +1000

    Change z-index

--- components/Overlay/index.tsx
+++ components/Overlay/index.tsx
@@ -18,7 +18,7 @@ const Overlay: FC<OProps> = styled(({ className, children }: OProps) => {
     top: 0;
     width: 100%;
     height: 100%;
-    z-index: 3;
+    z-index: 1;
 `;
 
 export default Overlay;
