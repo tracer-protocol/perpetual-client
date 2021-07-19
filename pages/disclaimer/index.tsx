6cbbc40ed6db5aa67408499d862f18c8dc0899d3
Tim Jones <t27.jones@connect.qut.edu.au>
2021-07-19 15:00:14 +1000

    Added full disclaimer text to page

--- pages/disclaimer/index.tsx
+++ pages/disclaimer/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import NavBar from '@components/Nav';
 import styled from 'styled-components';
 import Footer from '@components/Footer';
+import Link from 'next/link';
 
 const GeneralContainer = styled.div`
     max-height: calc(100vh - 100px);
@@ -17,6 +18,10 @@ const BodyText = styled.div`
 
     p:last-of-type {
         padding-bottom: 0px;
+    }
+
+    a {
+        color: var(--color-secondary);
     }
 `;
 const Title = styled.h1`
@@ -35,16 +40,17 @@ const SubHeading = styled.span`
     font-size: var(--font-size-small);
     color: var(--color-secondary);
     max-width: 720px;
-    margin-bottom: 16px;
+    margin-bottom: 8px;
 `;
 const SmallTitle = styled.h2`
     font-size: var(--font-size-medium);
     font-weight: 400;
     color: var(--color-text);
     max-width: 720px;
+    margin-bottom: 8px;
 `;
 const Text = styled.p`
-    padding: 16px 0px;
+    padding: 0px 0px 8px;
     font-size: var(--font-size-small);
     max-width: 720px;
 `;
@@ -53,28 +59,52 @@ const Disclaimer: React.FC = styled(({ className }) => {
         <div className={className}>
             <NavBar />
             <GeneralContainer className={`container`}>
-                <Title>Disclaimer</Title>
+                <Title>Tracer Protocol Disclaimer</Title>
                 <BodyText>
                     <SubHeading>
-                        Last Updated on July 8, 2021
+                        Last Updated on July 7, 2021
                     </SubHeading>
-                    <SmallTitle>
-                        Lorem Ipsum
-                    </SmallTitle>
                     <Text>
-                        Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit. Cras Gravida Egestas Nunc, A Pharetra Lorem. Vivamus Laoreet Tortor Leo, Ac Placerat Leo Sagittis Sit Amet. Maecenas Eget Velit Augue. Orci Varius Natoque Penatibus Et Magnis Dis Parturient Montes, Nascetur Ridiculus Mus. Quisque At Est Vitae Ante Gravida Fermentum Vitae Sit Amet Lectus. Aliquam Tincidunt Luctus Nulla Quis Accumsan. Cras Accumsan Felis Vel Nulla Facilisis Rutrum A In Magna. Cras Sit Amet Interdum Augue, Quis Laoreet Libero. Maecenas Finibus Est Nec Luctus Consectetur. Aenean Mi Tortor, Tempor Vel Purus Feugiat, Dignissim Imperdiet Ipsum. Integer Eleifend Sapien Sit Amet Malesuada Fermentum. Vestibulum Blandit Erat Non Ipsum Ultrices Semper.
+                        Your use of a smart contract governed by Tracer DAO, including Tracer Perpetual Swaps, (“Tracer protocol”) involves various risks, including, but not limited to, the risks outlined below. By interacting with the Tracer protocol, you accept those risks. If you choose to use the Tracer protocol, this Disclaimer applies in addition to Tracer’s Terms of Use, which are available <Link href="/terms-of-use/">here</Link>.
                     </Text>
                     <SmallTitle>
-                        Lorem Ipsum
+                        Price and asset risk: 
                     </SmallTitle>
                     <Text>
-                        Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit. Cras Gravida Egestas Nunc, A Pharetra Lorem. Vivamus Laoreet Tortor Leo, Ac Placerat Leo Sagittis Sit Amet. Maecenas Eget Velit Augue. Orci Varius Natoque Penatibus Et Magnis Dis Parturient Montes, Nascetur Ridiculus Mus. Quisque At Est Vitae Ante Gravida Fermentum Vitae Sit Amet Lectus. Aliquam Tincidunt Luctus Nulla Quis Accumsan. Cras Accumsan Felis Vel Nulla Facilisis Rutrum A In Magna. Cras Sit Amet Interdum Augue, Quis Laoreet Libero. Maecenas Finibus Est Nec Luctus Consectetur. Aenean Mi Tortor, Tempor Vel Purus Feugiat, Dignissim Imperdiet Ipsum. Integer Eleifend Sapien Sit Amet Malesuada Fermentum. Vestibulum Blandit Erat Non Ipsum Ultrices Semper.
+                        Asset losses are possible due to the fluctuation of prices of tokens. You acknowledge that financial contracts involving cryptocurrencies are inherently risky, some cryptocurrencies are not recognised legal tender in some countries, are unregulated by many central and government authorities, and may be subject to extreme price volatility. You warrant that you understand the risks associated with transactions, cryptocurrencies, financial contracts and any other goods, services or products in connection to the Tracer protocol. Before using the Tracer protocol, you should review the relevant documentation to make sure you understand how the Tracer protocol works. 
                     </Text>
                     <SmallTitle>
-                        Lorem Ipsum
+                        Liquidation risk:
                     </SmallTitle>
                     <Text>
-                        Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit. Cras Gravida Egestas Nunc, A Pharetra Lorem. Vivamus Laoreet Tortor Leo, Ac Placerat Leo Sagittis Sit Amet. Maecenas Eget Velit Augue. Orci Varius Natoque Penatibus Et Magnis Dis Parturient Montes, Nascetur Ridiculus Mus. Quisque At Est Vitae Ante Gravida Fermentum Vitae Sit Amet Lectus. Aliquam Tincidunt Luctus Nulla Quis Accumsan. Cras Accumsan Felis Vel Nulla Facilisis Rutrum A In Magna. Cras Sit Amet Interdum Augue, Quis Laoreet Libero. Maecenas Finibus Est Nec Luctus Consectetur. Aenean Mi Tortor, Tempor Vel Purus Feugiat, Dignissim Imperdiet Ipsum. Integer Eleifend Sapien Sit Amet Malesuada Fermentum. Vestibulum Blandit Erat Non Ipsum Ultrices Semper.
+                        When interacting with the Tracer protocol, your positions are subject to liquidation risk. Despite having well defined liquidation penalties, your loss could be 100% of your position. This risk is not only theoretical, during the so-called “Black Thursday”, around $8M of user’s positions in MakerDAO was lost. <b>The Tracer protocol is non-custodial, and therefore can not help users to avoid liquidations, or recover funds following liquidation.</b>
+                    </Text>
+                    <SmallTitle>
+                        Oracle risk:
+                    </SmallTitle>
+                    <Text>
+                        Tracer relies on oracles to provide spot price data for assets. This price data is used as part of the liquidation flow and, as a result, has inherent risks. Should oracle prices not be updated, or should they be updated erroneously, there is risk a user may be liquidated unexpectedly. At this time, the Tracer protocol uses Chainlink’s oracle network; however, this may be subject to change.
+                    </Text>
+                    <SmallTitle>
+                        Smart contract and software risk:
+                    </SmallTitle>
+                    <Text>
+                        The Tracer protocol has been audited by Sigma Prime and Code 423n4. The contracts were checked for correctness of functionality and safety of user funds. However, an audit does not eliminate the risk of an exploit or bug being present in the Tracer protocol. You must do your own research before interacting with the Tracer protocol, and only supply funds that you are prepared to lose.
+                    </Text>
+                    <SmallTitle>
+                        Contract upgrade risk:
+                    </SmallTitle>
+                    <Text>
+                        Contract upgrades may be implemented by Tracer DAO, via proposal, to the Tracer protocol. While these upgrades will likely be audited prior to implementation, the Tracer protocol’s codebase is subject to change and, as such, risks of exploits and bugs are present. You must remain aware of updates being implemented by Tracer DAO to the Tracer protocol, and ensure that, following any update, the Tracer protocol is safe to use.
+                    </Text>
+                    <SmallTitle>
+                        Governance risk:
+                    </SmallTitle>
+                    <Text>
+                        Tracer DAO owns and controls the Tracer protocol. Upgrades and modifications to the protocol are managed in a decentralised manner by holders of TCR governance tokens. No entity involved in creating the Tracer protocol (including developers) will be liable for any claims or damage whatsoever associated with your use, inability to use, direct, indirect, incidental, special, exemplary, punitive or consequential damages, or loss of profits, cryptocurrencies, tokens, or anything else of value. For the avoidance of doubt, Tracer DAO does not control user funds. 
+                    </Text>
+                    <Text>
+                        Discussions with updated information on the Tracer protocol’s governance can be found in <Link href="https://discourse.tracer.finance/">Tracer DAO's Discourse</Link>. 
                     </Text>
                 </BodyText>
             </GeneralContainer>
