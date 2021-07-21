ea1f1af7cfe2880857a22850d69b8a5cda76d981
StevenYulongYan <yulong.yan@uqconnect.edu.au>
2021-07-21 10:47:42 +1000

    Revert commenting out incorrect network banner

--- components/Nav/Navbar/index.tsx
+++ components/Nav/Navbar/index.tsx
@@ -9,72 +9,72 @@ import HeaderSiteSwitcher from './HeaderSiteSwitcher';
 import { useWeb3 } from '@context/Web3Context/Web3Context';
 import AccountDropdown from './AccountDropdown';
 
-// const switchNetworks = async () => {
-//     // @ts-ignore
-//     const ethereum = window.ethereum;
-//     try {
-//         await ethereum.request({
-//             method: 'wallet_switchEthereumChain',
-//             params: [{ chainId: '0x66EEB' }], //arbitrum
-//         });
-//     } catch (error) {
-//         // This error code indicates that the chain has not been added to MetaMask.
-//         if (error.code === 4902) {
-//             try {
-//                 await ethereum.request({
-//                     method: 'wallet_addEthereumChain',
-//                     params: [{ chainId: '0x66EEB', rpcUrl: 'https://rinkeby.arbitrum.io/rpc' }],
-//                 });
-//             } catch (addError) {
-//                 // handle "add" error
-//             }
-//         }
-//         // handle other "switch" errors
-//     }
-// };
-//
-// const NetworkButton = styled.span`
-//     border: 1px solid #fff;
-//     transition: 0.3s;
-//     border-radius: 20px;
-//     padding: 0 10px;
-//     &:hover {
-//         cursor: pointer;
-//         background: #fff;
-//         color: #f15025;
-//     }
-// `;
+const switchNetworks = async () => {
+    // @ts-ignore
+    const ethereum = window.ethereum;
+    try {
+        await ethereum.request({
+            method: 'wallet_switchEthereumChain',
+            params: [{ chainId: '0x66EEB' }], //arbitrum
+        });
+    } catch (error) {
+        // This error code indicates that the chain has not been added to MetaMask.
+        if (error.code === 4902) {
+            try {
+                await ethereum.request({
+                    method: 'wallet_addEthereumChain',
+                    params: [{ chainId: '0x66EEB', rpcUrl: 'https://rinkeby.arbitrum.io/rpc' }],
+                });
+            } catch (addError) {
+                // handle "add" error
+            }
+        }
+        // handle other "switch" errors
+    }
+};
 
-// type UNProps = {
-//     display: boolean;
-//     className?: string;
-// };
-// const UnknownNetwork: React.FC<UNProps> = styled(({ className }: UNProps) => {
-//     // TODO add an onclick to swap to arbritrum using
-//     // https://docs.metamask.io/guide/rpc-api.html#other-rpc-methods
-//     return (
-//         <div className={className}>
-//             You are connected to the wrong network. Switch to{' '}
-//             <NetworkButton onClick={() => switchNetworks()}>Arbitrum Testnet.</NetworkButton>
-//         </div>
-//     );
-// })`
-//     background: #f15025;
-//     color: var(--color-text);
-//     letter-spacing: -0.36px;
-//     height: 40px;
-//     line-height: 40px;
-//     font-size: var(--font-size-medium);
-//     width: 100%;
-//     position: absolute;
-//     left: 0;
-//     text-align: center;
-//     bottom: ${(props) => (props.display ? '-40px' : '0px')};
-//     opacity: ${(props) => (props.display ? '1' : '0')};
-//     z-index: ${(props) => (props.display ? '2' : '-1')};
-//     transition: ${(props) =>
-//         props.display ? 'bottom 0.3s, opacity 0.3s 0.1s' : 'bottom 0.3s 0.15s, opacity 0.3s, z-index 0.3s 0.3s'};
-// `;
+const NetworkButton = styled.span`
+    border: 1px solid #fff;
+    transition: 0.3s;
+    border-radius: 20px;
+    padding: 0 10px;
+    &:hover {
+        cursor: pointer;
+        background: #fff;
+        color: #f15025;
+    }
+`;
+
+type UNProps = {
+    display: boolean;
+    className?: string;
+};
+const UnknownNetwork: React.FC<UNProps> = styled(({ className }: UNProps) => {
+    // TODO add an onclick to swap to arbritrum using
+    // https://docs.metamask.io/guide/rpc-api.html#other-rpc-methods
+    return (
+        <div className={className}>
+            You are connected to the wrong network. Switch to{' '}
+            <NetworkButton onClick={() => switchNetworks()}>Arbitrum Testnet.</NetworkButton>
+        </div>
+    );
+})`
+    background: #f15025;
+    color: var(--color-text);
+    letter-spacing: -0.36px;
+    height: 40px;
+    line-height: 40px;
+    font-size: var(--font-size-medium);
+    width: 100%;
+    position: absolute;
+    left: 0;
+    text-align: center;
+    bottom: ${(props) => (props.display ? '-40px' : '0px')};
+    opacity: ${(props) => (props.display ? '1' : '0')};
+    z-index: ${(props) => (props.display ? '2' : '-1')};
+    transition: ${(props) =>
+        props.display ? 'bottom 0.3s, opacity 0.3s 0.1s' : 'bottom 0.3s 0.15s, opacity 0.3s, z-index 0.3s 0.3s'};
+`;
 
 const useEnsName = (account: string) => {
     const [ensName, setEnsName] = useState(account);
@@ -177,9 +177,9 @@ const NavBarContent: React.FC = styled(({ className }) => {
             />
 
             {/** TODO this will need to change to Arbritrum network id */}
-            {/*{process.env.NEXT_PUBLIC_DEPLOYMENT !== 'DEVELOPMENT' ? (*/}
-            {/*    <UnknownNetwork display={network !== 421611 && !!network} />*/}
-            {/*) : null}*/}
+            {process.env.NEXT_PUBLIC_DEPLOYMENT !== 'DEVELOPMENT' ? (
+                <UnknownNetwork display={network !== 421611 && !!network} />
+            ) : null}
         </nav>
     );
 })`
