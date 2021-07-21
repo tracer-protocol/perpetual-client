7d147137f2b4bfb69a9779972a05e20db631bf59
dospore <tetthera@gmail.com>
2021-07-21 22:34:48 +1000

    Fix build

--- pages/disclaimer/index.tsx
+++ pages/disclaimer/index.tsx
@@ -77,7 +77,7 @@ const Disclaimer: React.FC = styled(({ className }) => {
                     </Text>
                     <Text>
                         Discussions with updated information on the Tracer protocol{"'"}s governance can be found in{' '}
-                        <Link href="https://discourse.tracer.finance/">Tracer DAO{"'"}s Discourse</Link>.
+                        <Link href="https://discourse.tracer.finance/">{'Tracer DAO\'s Discourse'}</Link>.
                     </Text>
                 </BodyText>
             </GeneralContainer>
