194d2befde5f6803a6d97e03e2b8af845a657078
dospore <tetthera@gmail.com>
2021-07-23 11:31:13 +1000

    Update trader address

--- context/Web3Context/Web3Context.Config.ts
+++ context/Web3Context/Web3Context.Config.ts
@@ -36,7 +36,7 @@ export const networkConfig: Record<string, Network> = {
         previewUrl: 'https://rinkeby-explorer.arbitrum.io/#',
         contracts: {
             trader: {
-                address: '0x5A36DA9Ef7C090269F716791ea2CEE623A00dcaF',
+                address: '0xb6fEb22261c7cf27B7cc031f78575eCEE0778E48',
                 abi: traderJSON as AbiItem[],
             },
         },
