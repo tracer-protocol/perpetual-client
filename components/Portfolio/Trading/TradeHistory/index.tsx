55d53cc558e3b4ce7dfc491fe56fb5ef2a64eae8
Steven Yulong Yan <43001444+Steven-Yulong-Yan@users.noreply.github.com>
2021-07-27 13:16:43 +1000

    Positions and trade history tabs (#247)
    * Fetch trade history from filled orders
    
    * Move table elements
    
    * Remove unused tables
    
    * lint
    
    * Use allFilledOrders from FactoryStore
    
    * Remove extra filter
    
    * Add back filter
    
    * Update positions table headings
    
    * Remove tracer loading checks on positions table
    
    * Swap table cells

--- components/Portfolio/Trading/TradeHistory/index.tsx
+++ components/Portfolio/Trading/TradeHistory/index.tsx
@@ -1,28 +1,41 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { Logo } from '@components/General';
-import { TableHeading, TableRow, TableCell, Table } from '@components/Table';
-import { timeAgo, toApproxCurrency } from '@libs/utils';
+import { Table, TableHeader, TableBody, TableHeading, TableRow, TableCell } from '@components/Table';
+import { toApproxCurrency } from '@libs/utils';
+import { LabelledOrders } from '@libs/types/OrderTypes';
 
 interface THProps {
-    allFilledOrders: any;
+    fetchedTracers: any;
+    allFilledOrders: LabelledOrders;
 }
-const TradeHistory: FC<THProps> = ({ allFilledOrders }: THProps) => {
+const TradeHistory: FC<THProps> = ({ fetchedTracers, allFilledOrders }: THProps) => {
+    const [orderHistory, setOrderHistory] = useState<any>([]);
+
+    useEffect(() => {
+        const tempOrders: any[] = [];
+        fetchedTracers.map((tracer: any) => {
+            if (allFilledOrders[tracer.address] !== []) {
+                tempOrders.push(allFilledOrders[tracer.address]);
+            }
+        });
+        tempOrders.sort((order1, order2) => (order1.timestamp < order2.timestamp && 1) || -1);
+        setOrderHistory(tempOrders);
+    }, [fetchedTracers]);
+
     return (
         <>
             <Table>
-                <thead>
-                    <tr>
-                        {['Date', 'Market', 'Position', 'Exposure', 'Slippage', 'Fees', 'Total Cost', 'Order Type'].map(
-                            (heading: string, i: number) => (
-                                <TableHeading key={i}>{heading}</TableHeading>
-                            ),
-                        )}
-                    </tr>
-                </thead>
-                <tbody>
-                    {allFilledOrders?.map((order: any, i: number) => (
+                <TableHeader>
+                    {['Date', 'Market', 'Position', 'Exposure', 'Slippage', 'Fees', 'Total Cost', 'Order Type'].map(
+                        (heading: string, i: number) => (
+                            <TableHeading key={i}>{heading}</TableHeading>
+                        ),
+                    )}
+                </TableHeader>
+                <TableBody>
+                    {orderHistory?.map((order: any, i: number) => (
                         <TableRow key={`table-row-${i}`}>
-                            <TableCell>{timeAgo(Date.now(), parseInt(order?.timestamp) * 1000)}</TableCell>
+                            <TableCell>{String(new Date(parseInt(order?.timestamp)))}</TableCell>
                             <TableCell>
                                 <div className="flex flex-row">
                                     <div className="my-auto">
@@ -32,7 +45,7 @@ const TradeHistory: FC<THProps> = ({ allFilledOrders }: THProps) => {
                                 </div>
                             </TableCell>
                             <TableCell className={order?.position ? 'red' : 'green'}>
-                                {order?.position ? 'Short' : 'Long'}
+                                {order?.position ? 'SHORT' : 'LONG'}
                             </TableCell>
                             <TableCell>{order?.amount.toFixed(2)}</TableCell>
                             <TableCell>-</TableCell>
@@ -41,7 +54,7 @@ const TradeHistory: FC<THProps> = ({ allFilledOrders }: THProps) => {
                             <TableCell>-</TableCell>
                         </TableRow>
                     ))}
-                </tbody>
+                </TableBody>
             </Table>
         </>
     );
