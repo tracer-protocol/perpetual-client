75792728c49cd2ad49d209a751b3943e9d42a1fe
dospore <tetthera@gmail.com>
2021-07-28 15:12:22 +1000

    Add api codes

--- libs/Ome/messageMap.ts
+++ libs/Ome/messageMap.ts
@@ -1,16 +1,17 @@
+import { API_CODES  } from "@tracer-protocol/tracer-utils";
 // https://www.notion.so/tracerdao/4345ae29b90047be87637f760d5aa6a5?v=713a9e612b8847ffbd4a247223bea9dc
 
 export const errors: Record<string, string> = {
-    invalid_signature: 'Invalid signature',
-    under_margin: 'Not enough margin',
-    invalid_created_timestamp: 'Invalid creation timestamp',
-    invalid_expiry_timestamp: 'Invalid expiry timestamp',
-    not_whitelisted: 'Account not whitelisted',
+    [API_CODES.INVALID_SIGNATURE]: 'Invalid signature',
+    [API_CODES.UNDER_MARGIN]: 'Not enough margin',
+    [API_CODES.INVALID_CREATED_TIMESTAMP]: 'Invalid creation timestamp',
+    [API_CODES.INVALID_EXPIRY_TIMESTAMP]: 'Invalid expiry timestamp',
+    [API_CODES.NOT_WHITELISTED]: 'Account not whitelisted'
 };
 
 export const ok: Record<string, string> = {
-    placed: 'Order successfully placed',
-    fully_matched: 'Order fully matched',
-    partially_matched: 'Order partially matched',
-    cancelled: 'Order successfully cancelled',
+    [API_CODES.ORDER_PLACED]: 'Order successfully placed',
+    [API_CODES.ORDER_FULLY_MATCHED]: 'Order fully matched',
+    [API_CODES.ORDER_PARTIALLY_MATCHED]: 'Order partially matched',
+    [API_CODES.ORDER_CANCELLED]: 'Order successfully cancelled',
 };
