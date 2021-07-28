8964a5ea930377f27068b6b7c7a2cdd6920e80c8
dospore <tetthera@gmail.com>
2021-07-29 08:29:17 +1000

    Lint

--- libs/Ome/messageMap.ts
+++ libs/Ome/messageMap.ts
@@ -1,4 +1,4 @@
-import { API_CODES  } from "@tracer-protocol/tracer-utils";
+import { API_CODES } from '@tracer-protocol/tracer-utils';
 // https://www.notion.so/tracerdao/4345ae29b90047be87637f760d5aa6a5?v=713a9e612b8847ffbd4a247223bea9dc
 
 export const errors: Record<string, string> = {
@@ -6,7 +6,7 @@ export const errors: Record<string, string> = {
     [API_CODES.UNDER_MARGIN]: 'Not enough margin',
     [API_CODES.INVALID_CREATED_TIMESTAMP]: 'Invalid creation timestamp',
     [API_CODES.INVALID_EXPIRY_TIMESTAMP]: 'Invalid expiry timestamp',
-    [API_CODES.NOT_WHITELISTED]: 'Account not whitelisted'
+    [API_CODES.NOT_WHITELISTED]: 'Account not whitelisted',
 };
 
 export const ok: Record<string, string> = {
