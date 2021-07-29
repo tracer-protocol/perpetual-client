4b50d8e71d171b69c5ce26d9f893e37227e028b6
dospore <tetthera@gmail.com>
2021-07-29 15:29:03 +1000

    lint

--- libs/Ome/index.ts
+++ libs/Ome/index.ts
@@ -22,8 +22,8 @@ export const getOrders: (market: string) => Promise<Response> = async (market) =
     })
         .then((res) => res.json())
         .then((res) => {
-            if (res.data === "book_not_found") {
-                return []
+            if (res.data === 'book_not_found') {
+                return [];
             }
             console.debug('Fetched all orders', res?.data);
             return res?.data ?? [];
@@ -52,8 +52,8 @@ export const getUsersOrders: (market: string, account: string) => Promise<OMEOrd
     })
         .then((res) => res.json())
         .then((res) => {
-            if (res.data === "book_not_found") {
-                return []
+            if (res.data === 'book_not_found') {
+                return [];
             } else if (res.status === 404) {
                 console.error('Failed to fetch user orders', res);
                 return [];
