79eb1667c97bb3a71ed6ab70613771f63708307a
dospore <tetthera@gmail.com>
2021-07-30 15:03:40 +1000

    Fix build

--- components/Charts/AdvancedTrading/index.tsx
+++ components/Charts/AdvancedTrading/index.tsx
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic';
 
-const TVChartContainer = dynamic(
+const TVChartContainer: any = dynamic(
     // @ts-ignore
     import('./TVChartContainer'),
     {
--- components/Trade/Advanced/RightPanel/Graph/index.tsx
+++ components/Trade/Advanced/RightPanel/Graph/index.tsx
@@ -1,8 +1,6 @@
 import React, { FC, useState } from 'react';
 import styled from 'styled-components';
 // import { useCandles } from '@libs/Graph/hooks/Tracer';
-// import { CandleData } from 'libs/types/TracerTypes';
-// import CandleStickChart from '@components/Charts/CandleStickChart';
 import FogOverlay from '@components/Overlay/FogOverlay';
 import TVChartContainer from '@components/Charts/AdvancedTrading';
 
@@ -13,17 +11,14 @@ const GraphContent = styled.div`
 
 interface GProps {
     className?: string;
-    selectedTracerAddress: string;
 }
-const Graph: FC<GProps> = styled(({ selectedTracerAddress, className }: GProps) => {
+const Graph: FC<GProps> = styled(({ className }: GProps) => {
     // const { candles } = useCandles(selectedTracerAddress);
-    console.log(selectedTracerAddress);
     const [showOverlay, setOverlay] = useState(true);
     return (
         <div className={className}>
             <GraphContent>
                 <TVChartContainer />
-                {/* <CandleStickChart candleData={candles as CandleData} /> */}
             </GraphContent>
             {showOverlay ? <FogOverlay buttonName="Show Chart" onClick={() => setOverlay(false)} /> : null}
         </div>
