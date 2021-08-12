1ee9debb3493d167b9eceb420745a7b884648ecf
Steven Yulong Yan <yulong.yan@uqconnect.edu.au>
2021-08-13 00:22:58 +1000

    Remove default logo

--- components/General/index.tsx
+++ components/General/index.tsx
@@ -135,26 +135,31 @@ export const Section: React.FC<SProps> = styled(({ className, children, label, t
 `;
 
 const clearLogos: Record<string, string> = {
+    TSLA: '/img/logos/currencies/tesla_clear.svg',
     ETH: '/img/logos/currencies/eth_clear.svg',
-    TEST1: '/img/logos/currencies/eth_clear.svg',
+    LINK: '/img/logos/currencies/link_clear.svg',
 };
 
 const logos: Record<string, string> = {
     TSLA: '/img/logos/currencies/tesla.svg',
     ETH: '/img/logos/currencies/eth.svg',
-    TEST1: '/img/logos/currencies/eth.svg',
     LINK: '/img/logos/currencies/link.svg',
-    DEFAULT: '/img/logos/currencies/tesla.svg',
 };
 
 interface LProps {
     className?: string;
-    ticker: string;
+    ticker: string | undefined;
     clear?: boolean; // true then display outlined image
 }
 
 export const Logo: React.FC<LProps> = styled(({ className, ticker, clear }: LProps) => {
-    return <img className={className} src={clear ? clearLogos[ticker] : logos[ticker] ?? logos['TSLA']} alt="logo" />;
+    return (
+        <>
+            {(ticker === 'TSLA' || 'ETH' || 'LINK') && ticker !== undefined ? (
+                <img className={className} src={clear ? clearLogos[ticker] : logos[ticker]} alt="logo" />
+            ) : null}
+        </>
+    );
 })`
     width: 30px;
     margin: 5px 0;
