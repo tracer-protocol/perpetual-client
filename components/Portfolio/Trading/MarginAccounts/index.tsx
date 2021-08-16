24a2f22987b766e528eea83f8de2318fb15016be
Steven Yulong Yan <43001444+Steven-Yulong-Yan@users.noreply.github.com>
2021-08-16 15:12:05 +1000

    Margin deposit withdraw (#284)
    * Add account modal to margin accounts
    
    * Move account modal
    
    * Update

--- components/Portfolio/Trading/MarginAccounts/index.tsx
+++ components/Portfolio/Trading/MarginAccounts/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Logo } from '@components/General';
 import { toApproxCurrency, toPercent } from '@libs/utils';
 import {
@@ -12,65 +12,94 @@ import {
     TableHeader,
 } from '@components/General/Table';
 import { StatusIndicator, calcStatus } from '@components/Portfolio';
-import Tooltip from 'antd/lib/tooltip';
-import Tracer from '@libs/Tracer';
+import Tracer, { defaults } from '@libs/Tracer';
+import AccountModal from '@components/General/TracerModal/AccountModal';
 
-const NoLeverageTip = <p>You have no leveraged trades open in this market.</p>;
+// const NoLeverageTip = <p>You have no leveraged trades open in this market.</p>;
 
 const MarginAccounts: React.FC<{
     tracers: Tracer[];
 }> = ({ tracers }) => {
+    const [popup, setPopup] = useState(false);
+    const [deposit, setDeposit] = useState(false);
+    const [tracer, setTracer] = useState<Tracer>();
+
+    const handleClick = (popup: boolean, deposit: boolean, tracer: Tracer) => {
+        setPopup(popup);
+        setDeposit(deposit);
+        setTracer(tracer);
+    };
+
     const headings = ['Market', 'Equity', 'Maintenance Margin', 'Available Margin', 'Status of Position'];
 
     return (
-        <Table>
-            <TableHeader>
-                <tr>
-                    {headings.map((heading, i) =>
-                        i !== 4 ? (
-                            <TableHeading>{heading}</TableHeading>
-                        ) : (
-                            <TableLastHeading>{heading}</TableLastHeading>
-                        ),
-                    )}
-                </tr>
-            </TableHeader>
-            <TableBody>
-                {tracers.map((tracer, i) => {
-                    const balances = tracer.getBalance();
-                    const status = calcStatus(balances.base.toNumber(), balances.availableMarginPercent.toNumber());
-                    return (
-                        <TableRow key={`table-row-${i}`}>
-                            <TableCell>
-                                <Tooltip title={NoLeverageTip}>
+        <>
+            <Table>
+                <TableHeader>
+                    <tr>
+                        {headings.map((heading, i) =>
+                            i !== 4 ? (
+                                <TableHeading>{heading}</TableHeading>
+                            ) : (
+                                <TableLastHeading>{heading}</TableLastHeading>
+                            ),
+                        )}
+                    </tr>
+                </TableHeader>
+                <TableBody>
+                    {tracers.map((tracer, i) => {
+                        const balances = tracer.getBalance();
+                        const status = calcStatus(balances.base.toNumber(), balances.availableMarginPercent.toNumber());
+                        return (
+                            <TableRow key={`table-row-${i}`}>
+                                <TableCell>
+                                    {/*<Tooltip title={NoLeverageTip}>*/}
                                     <div className="flex flex-row">
                                         <div className="my-auto">
                                             <Logo ticker={tracer.baseTicker} />
                                         </div>
                                         <div className="my-auto ml-2">{tracer.marketId}</div>
                                     </div>
-                                </Tooltip>
-                            </TableCell>
-                            <TableCell>{toApproxCurrency(balances.totalMargin)}</TableCell>
-                            <TableCell>{toApproxCurrency(balances.minimumMargin)}</TableCell>
-                            <TableCell>{toPercent(balances.availableMarginPercent.toNumber(), true)}</TableCell>
-                            <TableLastCell>
-                                <div className="flex flex-row">
-                                    <StatusIndicator color={status.color} className="text-2xl my-auto">
-                                        &bull;
-                                    </StatusIndicator>
-                                    <div className="mx-2 my-auto">{status.text}</div>
-                                    <div className="flex flex-row my-auto ml-auto mr-4">
-                                        <Button className="mr-2">Deposit</Button>
-                                        <Button>Withdraw</Button>
+                                    {/*</Tooltip>*/}
+                                </TableCell>
+                                <TableCell>{toApproxCurrency(balances.totalMargin)}</TableCell>
+                                <TableCell>{toApproxCurrency(balances.minimumMargin)}</TableCell>
+                                <TableCell>{toPercent(balances.availableMarginPercent.toNumber(), true)}</TableCell>
+                                <TableLastCell>
+                                    <div className="flex flex-row">
+                                        <StatusIndicator color={status.color} className="text-2xl my-auto">
+                                            &bull;
+                                        </StatusIndicator>
+                                        <div className="mx-2 my-auto">{status.text}</div>
+                                        <div className="flex flex-row my-auto ml-auto mr-4">
+                                            <Button
+                                                className="mr-2"
+                                                onClick={(_e: any) => handleClick(true, true, tracer)}
+                                            >
+                                                Deposit
+                                            </Button>
+                                            <Button onClick={(_e: any) => handleClick(true, false, tracer)}>
+                                                Withdraw
+                                            </Button>
+                                        </div>
                                     </div>
-                                </div>
-                            </TableLastCell>
-                        </TableRow>
-                    );
-                })}
-            </TableBody>
-        </Table>
+                                </TableLastCell>
+                            </TableRow>
+                        );
+                    })}
+                </TableBody>
+            </Table>
+            <AccountModal
+                display={popup}
+                close={() => setPopup(false)}
+                isDeposit={deposit}
+                setDeposit={setDeposit}
+                unit={tracer?.marketId?.split('/')[1] ?? 'NO_ID'}
+                balances={tracer?.getBalance() ?? defaults.balances}
+                maxLeverage={tracer?.getMaxLeverage() ?? defaults.maxLeverage}
+                fairPrice={tracer?.getFairPrice() ?? defaults.fairPrice}
+            />
+        </>
     );
 };
 
